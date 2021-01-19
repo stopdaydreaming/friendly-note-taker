dab3be7525da06861a21a0c411b2047095ba3107
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-18 23:09:59 -0500

    updated routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,5 +11,10 @@ module.exports = function(app) {
     app.post("/api/notes", function(req, res) {
         db.push(req.body);
         res.json(true);
-    })
+    });
+
+    //delete data
+    app.delete("/api/notes/id", function(req, res) {
+        console.log("delete a note");
+    });
 }
